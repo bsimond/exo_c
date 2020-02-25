faba0254512ce2b16fbd3c66ef73b1341bd79917
bsimond <brice.simond@le-campus-numerique.fr>
2020-02-25 16:32:55 +0100

    création d un calcul pour trouver la distance entre deux point

--- 71_distance_point.c/src/main.c
+++ 71_distance_point.c/src/main.c
@@ -1,38 +1,29 @@
-/*
-*****************************************************************************
 
-  File        : main.cpp
-
-  The MIT License (MIT)
-  Copyright (c) 2019 STMicroelectronics
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-
-*****************************************************************************
-*/
 
 #include<stdio.h>
+#include <math.h>
 
 int main(void){
 
-	printf("Hello World!\n");
-	fflush(stdout);
+int position_1x=30;
+int position_1y=38;
+int position_2x=45;
+int position_2y=48;
+double distance_y;
+double distance_x;
+double distance_tt;
+
+distance_x = (position_1x - position_2x);
+distance_x = distance_x * distance_x;
+
+distance_y= position_1y - position_2y;
+distance_y= distance_y * distance_y;
+
+distance_tt=  sqrt(distance_x + distance_y);
+
+
+printf("%.2f",distance_tt);
 
 	return 0;
+
 }
